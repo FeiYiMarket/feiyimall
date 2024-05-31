c02c59c5f2f20976fd8e2b4fba79e8d58db377c5
Ping.Chen <chp0524@gmail.com>
2024-05-31 10:37:59 +0800

    update

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  version: '1.1.0',
+  version: '1.1.1',
   note: '首次版本发布', // 这个为版本描述，无需修改 
   subDomain: '3201354e726fcd40c8c8bb3c9b38c336', // liu123 此处改成你自己的专属域名。什么是专属域名？请看教程 https://www.it120.cc/help/qr6l4m.html
   merchantId: 63988, // 商户ID，可在后台工厂设置-->商户信息查看
